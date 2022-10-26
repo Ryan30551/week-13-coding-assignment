b194be0b192fe406864079d01f983201a6fc3e13
github_Ryan30551 <patrickryanf@gmail.com>
2022-10-26 02:05:03 -0500

    first commit

---
+++ src/components/loginform.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./loginform.css"
+
+const LoginForm = () => {
+    return (
+        <div className="cover">
+            <h3>Log In</h3>
+            <input type="text" placeholder="username" />
+            <input type="password" placeholder="password" />
+
+            <div className="login-btn">Login</div>
+
+            <p className="reset-btn">Reset Login</p>
+        </div>
+    )
+}
+
+export default LoginForm
